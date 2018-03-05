ee5d24cc5267240f094587d8cdc660c405a88aab
briankeegan <brian.morris.keegan@gmail.com>
2018-03-05 18:45:12 -0500

    Update root to display Sections
    -  Root will display Section resource, and all it's sub-sequential data

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -1,15 +1,25 @@
 'use strict'
 
 const controller = require('lib/wiring/controller')
+const models = require('app/models')
+const Section = models.section
 
 /* GET home page. */
-const root = (req, res) => {
-  res.json({
-    index: {
-      title: 'Express Template',
-      environment: req.app.get('env')
-    }
-  })
+const root = (req, res, next) => {
+  Section.find()
+    .populate({ path: 'walls',
+      populate: {
+        path: 'climbingRoutes',
+        populate: {
+          path: 'ratings'
+        }
+      }
+    })
+    .then(sections => res.json({
+      sections: sections.map((e) =>
+        e.toJSON({ virtuals: true, user: req.user }))
+    }))
+    .catch(next)
 }
 
 module.exports = controller({
